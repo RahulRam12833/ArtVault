2e6efdd584784dfcd00ed913bf758bb3e636eb99
Rahul Ram <ramr12833@gmail.com>
2025-05-28 14:22:02 +0930

    Add Firestore configuration and initialization

--- artvault/src/utilities/firebase/firebase.utils.js
+++ artvault/src/utilities/firebase/firebase.utils.js
@@ -1,6 +1,8 @@
 import {initializeApp} from 'firebase/app'
 import {getAuth,signInWithRedirect,signInWithPopup, GoogleAuthProvider} from 'firebase/auth';
 
+import {getFirestore,doc,getDoc,setDoc} from 'firebase/firestore';
+
 const firebaseConfig = {
   apiKey: "AIzaSyBZrVU30Tdd5anjm8psQ89WrFAWHJVbXfA",
   authDomain: "artvault-db.firebaseapp.com",
@@ -22,3 +24,28 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const dataBase = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(dataBase, 'users', userAuth.uid);
+
+  const userSnapshot = await getDoc(userDocRef);
+  // If user data does not exist, create it
+
+  if (!userSnapshot.exists()) {
+    const {displayName, email} = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt
+      });
+    } catch (error) {
+      console.error("Error creating the user", error);
+    }
+  }
+  return userDocRef;
+}
