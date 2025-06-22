ba23f66663fd23aef0ac4d6e0d5223fd70576b34
Rahul Ram <ramr12833@gmail.com>
2025-06-22 11:33:03 +0930

    add memoized selectors using Reselect for categories state

--- artvault/src/store/categories/category.selector.js
+++ artvault/src/store/categories/category.selector.js
@@ -1,6 +1,19 @@
-export const selectCategoriesMap = (state) => 
-    state.categories.categories.reduce((acc,category)=>{ 
-    const {title,items}=category;
-    acc[title.toLowerCase()]=items;
-    return acc;
-  },{})
+import { createSelector } from 'reselect';
+
+const selectCategoriesReducer = (state) => state.categories;
+
+export const selectCategories = createSelector(
+  [selectCategoriesReducer],  
+  (categoriesSlice)=>categoriesSlice.categories //run this only once when categoriesSlice changes
+)
+
+
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categories) =>      //run this only once when categories changes
+    categories.reduce((acc, category) => {
+      const { title, items } = category;
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {})
+)
