969b91c81d4e0dd5dc0a7749c6b3ce6946244b4c
Rahul Ram <ramr12833@gmail.com>
2025-06-26 11:09:37 +0930

    Install redux toolkit and configure intial store

--- artvault/src/main.jsx
+++ artvault/src/main.jsx
@@ -11,11 +11,11 @@ import './index.scss'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
+      {/* <PersistGate loading={null} persistor={persistor}> */}
         <BrowserRouter>
             <App />
         </BrowserRouter>
-      </PersistGate>
+      {/* </PersistGate> */}
     </Provider>
   </StrictMode>,
 )
