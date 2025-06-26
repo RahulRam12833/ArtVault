d3017ce1f1e4198806db6862535e511c0a59f165
Rahul Ram <ramr12833@gmail.com>
2025-06-26 17:30:49 +0930

    Create User slice with inital setup

--- artvault/src/main.jsx
+++ artvault/src/main.jsx
@@ -2,10 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {BrowserRouter} from 'react-router-dom'
 import {Provider} from 'react-redux'
-import { PersistGate } from 'redux-persist/integration/react'
 
 import App from './App.jsx'
-import { store ,persistor} from './store/store.js'
+import { store} from './store/store.js'
 import './index.scss'
 
 createRoot(document.getElementById('root')).render(
