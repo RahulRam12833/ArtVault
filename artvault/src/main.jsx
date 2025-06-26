95e3b194ee6ac45ea33fe9d3aef4e1c1b39d665b
Rahul Ram <ramr12833@gmail.com>
2025-06-27 00:37:52 +0930

    Install Stripe packages and initialize stripe loading

--- artvault/src/main.jsx
+++ artvault/src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {BrowserRouter} from 'react-router-dom'
+import {Elements} from '@stripe/react-stripe-js'
 import {Provider} from 'react-redux'
 
 import App from './App.jsx'
 import { store} from './store/store.js'
+import { stripePromise } from './utilities/stripe/stripe.utils.js'
 import './index.scss'
 
 createRoot(document.getElementById('root')).render(
@@ -12,7 +14,9 @@ createRoot(document.getElementById('root')).render(
     <Provider store={store}>
       {/* <PersistGate loading={null} persistor={persistor}> */}
         <BrowserRouter>
+          <Elements stripe={stripePromise}>
             <App />
+          </Elements>
         </BrowserRouter>
       {/* </PersistGate> */}
     </Provider>
