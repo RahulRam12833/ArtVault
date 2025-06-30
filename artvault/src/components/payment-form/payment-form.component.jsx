eb4caa6d734de884d9bf99b983ddba0ab9cf3617
Rahul Ram <ramr12833@gmail.com>
2025-06-30 16:33:26 +0930

    Set up Netlify CLI and Stripe payment with dummy data

--- artvault/src/components/payment-form/payment-form.component.jsx
+++ artvault/src/components/payment-form/payment-form.component.jsx
@@ -1,10 +1,11 @@
-import {CardElement} from '@stripe/react-stripe-js'
+import {useStripe,useElements,CardElement} from '@stripe/react-stripe-js'
 
 import Button,{BUTTON_TYPES} from '../button/button.component'
 
 import {PaymentFormContainer,FormContainer} from './payment-form.styles'
 const PaymentForm=()=>{
-
+    const stripe=useStripe();
+    const elements=useElements();
     const paymentHandler=async(e)=>{
         e.preventDefault();
 
@@ -12,16 +13,43 @@ const PaymentForm=()=>{
             return
         }
 
-        
+        const response=await fetch('/.netlify/functions/create-payment-intent',{
+            method:'post',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify({amount:10000})
+        }).then(res=>res.json())
 
+        const clientSecret=response.paymentIntent.client_secret
+
+        console.log(clientSecret)
+
+        const paymentResult=await stripe.confirmCardPayment(clientSecret,{
+            payment_method:{
+                card:elements.getElement(CardElement),
+                billing_details:{
+                    name:'Rahul Ram'
+                }
+            }
+        })
+
+        if(paymentResult.error){
+            alert(paymentResult.error)
+        }
+        else{
+            if(paymentResult.paymentIntent.status==="succeeded"){
+                alert("Payment Successful")
+            }
+        }
     }
 
     return(
         <PaymentFormContainer>
             <FormContainer>
                 <h2>Credit Cart Payment:</h2>
-                 <CardElement/>
-                 <Button buttonType={BUTTON_TYPES.inverted}>Pay Now</Button>
+                 <CardElement options={{hidePostalCode:true}}/>
+                 <Button buttonType={BUTTON_TYPES.inverted} onClick={paymentHandler}>Pay Now</Button>
             </FormContainer>
         </PaymentFormContainer>
     )
