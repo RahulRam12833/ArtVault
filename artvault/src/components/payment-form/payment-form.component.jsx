6393515caf7a7f1f64c34b8a08cd3ebae59073aa
Rahul Ram <ramr12833@gmail.com>
2025-06-30 18:43:53 +0930

    Refactor Stripe payment to use User data

--- artvault/src/components/payment-form/payment-form.component.jsx
+++ artvault/src/components/payment-form/payment-form.component.jsx
@@ -1,11 +1,20 @@
 import {useStripe,useElements,CardElement} from '@stripe/react-stripe-js'
+import { useState } from 'react'
+import {useSelector} from 'react-redux'
 
+import { selectCartTotal } from '../../store/cart/cart.selector'
+import { selectCurrentUser } from '../../store/user/user.selector'
 import Button,{BUTTON_TYPES} from '../button/button.component'
 
 import {PaymentFormContainer,FormContainer} from './payment-form.styles'
 const PaymentForm=()=>{
     const stripe=useStripe();
     const elements=useElements();
+
+    const amount=useSelector(selectCartTotal);
+    const currentUser=useSelector(selectCurrentUser)
+    const [isProcessingPayment,setIsProcessingPayment]=useState(false)
+
     const paymentHandler=async(e)=>{
         e.preventDefault();
 
@@ -13,26 +22,29 @@ const PaymentForm=()=>{
             return
         }
 
+        setIsProcessingPayment(true)
         const response=await fetch('/.netlify/functions/create-payment-intent',{
             method:'post',
             headers:{
                 'Content-Type':'application/json'
             },
-            body:JSON.stringify({amount:10000})
+            body:JSON.stringify({amount:amount*100})
         }).then(res=>res.json())
 
         const clientSecret=response.paymentIntent.client_secret
-
         console.log(clientSecret)
 
         const paymentResult=await stripe.confirmCardPayment(clientSecret,{
             payment_method:{
                 card:elements.getElement(CardElement),
                 billing_details:{
-                    name:'Rahul Ram'
+                    name:currentUser?currentUser.displayName:'Guest',
+
                 }
             }
         })
+
+        setIsProcessingPayment(false)
 
         if(paymentResult.error){
             alert(paymentResult.error)
@@ -49,7 +61,7 @@ const PaymentForm=()=>{
             <FormContainer>
                 <h2>Credit Cart Payment:</h2>
                  <CardElement options={{hidePostalCode:true}}/>
-                 <Button buttonType={BUTTON_TYPES.inverted} onClick={paymentHandler}>Pay Now</Button>
+                 <Button disabled={isProcessingPayment} buttonType={BUTTON_TYPES.inverted} onClick={paymentHandler}>Pay Now</Button>
             </FormContainer>
         </PaymentFormContainer>
     )
