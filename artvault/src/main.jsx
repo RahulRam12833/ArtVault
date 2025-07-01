168e700703838dd92dc29b80bd3e877af37757d7
Rahul Ram <ramr12833@gmail.com>
2025-07-01 20:18:55 +0930

    fix(store): re-enable Redux Persist integration to restore state persistence

--- artvault/src/main.jsx
+++ artvault/src/main.jsx
@@ -3,22 +3,22 @@ import { createRoot } from 'react-dom/client'
 import {BrowserRouter} from 'react-router-dom'
 import {Elements} from '@stripe/react-stripe-js'
 import {Provider} from 'react-redux'
-
+import {PersistGate} from 'redux-persist/integration/react'
 import App from './App.jsx'
-import { store} from './store/store.js'
+import { store,persistor} from './store/store.js'
 import { stripePromise } from './utilities/stripe/stripe.utils.js'
 import './index.scss'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      {/* <PersistGate loading={null} persistor={persistor}> */}
+      <PersistGate loading={null} persistor={persistor}> 
         <BrowserRouter>
           <Elements stripe={stripePromise}>
             <App />
           </Elements>
         </BrowserRouter>
-      {/* </PersistGate> */}
+      </PersistGate>
     </Provider>
   </StrictMode>,
 )
--- artvault/src/store/store.js
+++ artvault/src/store/store.js
@@ -1,31 +1,31 @@
 // import {compose,createStore,applyMiddleware} from 'redux';
-// import {persistStore,persistReducer} from 'redux-persist';
-// import storage from 'redux-persist/lib/storage'; 
+import {persistStore,persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; 
 import {configureStore} from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import { rootReducer } from './root-reducer';
 
-// const persistConfig = {
-//     key: 'root',
-//     storage,
-//     blacklist: ['user']
+const persistConfig = {
+    key: 'root',
+    storage,
+    blacklist: ['user']
 
-// }
+}
 
 const middleWares = [import.meta.env.MODE ==='development' && logger].filter(Boolean);
 
 
 // const composeEnhancer=(import.meta.env.MODE === 'development' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
-// const persistedReducer = persistReducer(persistConfig, rootReducer);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 
 
 // const composeEnhancers=composeEnhancer(applyMiddleware(...middleWares));
 
 export const store = configureStore({
-    reducer: rootReducer,
+    reducer: persistedReducer,
     middleware:(getDefaultMiddleware)=>getDefaultMiddleware().concat(middleWares),
 })
 // export const store = createStore(persistedReducer,undefined, composeEnhancers)
-// export const persistor = persistStore(store);
+export const persistor = persistStore(store);
